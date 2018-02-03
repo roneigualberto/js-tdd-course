12e1ac6dbca496d88b13d008e20989d53f5db70e
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-02-03 11:08:35 -0400

    Realizando asserções com chai

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,55 +1,70 @@
+const expect = require('chai').expect;
+
+
 describe('Main', function () {
+
 
     let arr;
 
     // Roda Uma vez, antes do bloco
     before(function () {
-       
+
     });
 
     // Roda Uma vez, depois do bloco
     after(function () {
-      
+
     });
 
     // Roda todas as vezes, antes de cada bloco
     beforeEach(function () {
-         arr = [1,2,3];
+        arr = [1, 2, 3];
     });
     // Roda todas as vezes, depois de cada bloco
-    afterEach(function(){
-       
+    afterEach(function () {
+
     });
 
-  
+    it('should be an array', function() {
+        expect(arr).to.be.a('array');
+    })
 
-    it('should have a size of 4 when push another value to the array', function(){
 
-      
+
+    it('should have a size of 4 when push another value to the array', function () {
+
+
         arr.push(4);
 
-        console.log(arr.length);
+
+        expect(arr).to.have.lengthOf(4);
+
+
 
     });
 
-    it('should have a size of 2 when pop a value from the array', function(){
-      
+    it('should have a size of 2 when pop a value from the array', function () {
 
-        arr.push();
 
-        console.log(arr.length);
+        arr.pop();
+
+     
+
+        expect(arr).to.have.lengthOf(2);
     });
 
 
-    it('should remove the value 3 when use pop in the array', function(){
-      
+    it('should remove the value 3 when use pop in the array', function () {
+
+
+        arr.pop();
+
+        expect(arr).to.not.include(3);
 
        
-
-        console.log( arr.pop() === 3);
     });
 
-    
+
 
 
 
