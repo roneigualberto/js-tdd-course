023b1fbea1eae71214dc6ea5cb2d4a5066ef0060
Ronei Gualberto <ronei.mac.rmg@gmail.com>
2018-02-03 10:55:19 -0400

    Utilizando hooks na prática

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,7 @@
 describe('Main', function () {
 
+    let arr;
+
     // Roda Uma vez, antes do bloco
     before(function () {
        
@@ -12,17 +14,18 @@ describe('Main', function () {
 
     // Roda todas as vezes, antes de cada bloco
     beforeEach(function () {
-        
+         arr = [1,2,3];
     });
     // Roda todas as vezes, depois de cada bloco
     afterEach(function(){
        
     });
 
+  
+
     it('should have a size of 4 when push another value to the array', function(){
 
-        var arr = [1,2,3];
-
+      
         arr.push(4);
 
         console.log(arr.length);
@@ -30,12 +33,23 @@ describe('Main', function () {
     });
 
     it('should have a size of 2 when pop a value from the array', function(){
-        var arr = [1,2,3];
+      
 
         arr.push();
 
         console.log(arr.length);
     });
+
+
+    it('should remove the value 3 when use pop in the array', function(){
+      
+
+       
+
+        console.log( arr.pop() === 3);
+    });
+
+    
 
 
 
